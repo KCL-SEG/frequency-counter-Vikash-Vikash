163d4db2d2e742eb645b42e6d380b270a91d2ecf
Vikash <viksk@VIKASH-LAPTOP.localdomain>
2022-09-30 17:58:11 +0100

    implemented functionality

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,14 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    for i in items:
+        i = str(i)
+        if i in frequencies:
+            current = frequencies[i]
+            current+=1
+            frequencies[i]=current
+        else:
+            frequencies[i]=1
+
     return frequencies
